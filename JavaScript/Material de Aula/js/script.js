1e06b63528127f4a73af15961100c6ff0d10ed01
Dan-NGomes <162141530+Dan-NGomes@users.noreply.github.com>
2024-04-18 09:56:37 -0300

    aula1JavaScript
    introdução

---
+++ JavaScript/Material de Aula/js/script.js
@@ -0,0 +1,15 @@
+console.log("Olá mundo")
+
+//caixas de texto (input)
+//prompt=recebe um texto
+//window.prompt("Qual é o seu nome?");
+
+//alert= manda uma mensagem
+//window.alert("FRONT É TOP!");
+
+//confirm= pergunta podendo sair verdadeiro ou falso
+//Window.confirm("Deseja baixar aequivo novamente?");
+
+var melancia = "uma variável de teste";
+var ligado = true;
+
