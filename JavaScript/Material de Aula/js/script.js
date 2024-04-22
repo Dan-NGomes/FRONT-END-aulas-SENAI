530e70d7eff1c64147cf8699f05e8e9d68333459
Dan-NGomes <162141530+Dan-NGomes@users.noreply.github.com>
2024-04-22 09:54:22 -0300

    javaScript 1

--- JavaScript/Material de Aula/js/script.js
+++ JavaScript/Material de Aula/js/script.js
@@ -10,6 +10,55 @@ console.log("Olá mundo")
 //confirm= pergunta podendo sair verdadeiro ou falso
 //Window.confirm("Deseja baixar aequivo novamente?");
 
-var melancia = "uma variável de teste";
+var melancia = "uma variável de teste!";
 var ligado = true;
+var numero = 91;
+var objeto =[];
+// objeto = tió coletanea de dados, onde podemos inserir,remover  e pesquisar
+
+//type of = verifica o tipo de variável
+
+//const = constante , uma variavel que não pode ser subscrita
+const fixo = "Não posso mudar!";
+var temporarioVar = "Fico Salvo!";
+let temporaria = "sou temporario!";
+
+function teste(){
+    temporaria ="Rodando no function Let";
+    temporárioVar = "Rodando no function";
+
+    nome = "Victor";
+
+    console.log(temporaria);
+    console.log(temporarioVar);
+}
+
+
+//output = resultado
+//input = numero1, numero2
+//Coleta dois numeros  e soma eles, após isso, imprime na tela  resultado
+
+function somar(){
+    window.alert("Seja bem vindo ao algoritmo de somar!");
+    let numero1 = Number(window.prompt ("Digite o seu primeiro número:"));
+    let numero2 = Number(window.prompt ("Digite seu segundo número:"));
+    //ParseInt = Números Inteiros
+    //ParseFloat = Números reais
+    //Number = Números inteiros e reais
+
+    //window.alert (type of  numero1)
+
+    window.alert (typeof numero1)
+
+    var resultado = numero1 + numero2;
+
+    //concatenação
+    window.alert("Resultado é de :"+resultado)
+    window.alert(`Resultado é de : ${resultado}`);
+
+}
+
+somar()
+
+
 
